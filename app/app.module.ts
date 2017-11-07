568ee4115de8fe37a631040c553d47bc33aeabb4
Ramdhas Ueikattan <ramdhas.ueikattan@syncfusion.com>
2017-11-07 14:24:51 +0530

    Add files via upload

---
+++ app/app.module.ts
@@ -0,0 +1,13 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { GridModule,SelectionService, PageService, SortService, FilterService, ToolbarService, EditService } from '@syncfusion/ej2-ng-grids';
+import { ButtonModule } from '@syncfusion/ej2-ng-buttons';
+import { AppComponent }  from './app.component';
+
+@NgModule({
+  imports:      [ BrowserModule, GridModule, ButtonModule ],
+  declarations: [ AppComponent ],
+  bootstrap:    [ AppComponent ],
+  providers: [PageService, SelectionService, SortService, ToolbarService, FilterService, EditService]
+})
+export class AppModule { }
